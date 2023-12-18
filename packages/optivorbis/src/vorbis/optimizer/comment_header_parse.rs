3de77157337d49c1ec2816fc83bd09cdf2517189
Alejandro González <me@alegon.dev>
2023-12-18 19:13:30 +0100

    chore: fix new Clippy lints

--- packages/optivorbis/src/vorbis/optimizer/comment_header_parse.rs
+++ packages/optivorbis/src/vorbis/optimizer/comment_header_parse.rs
@@ -69,105 +69,17 @@ impl CommentHeaderParse {
 			}
 		};
 
-		macro_rules! get_packet_checked {
-			($index:expr) => {
-				comment_header
-					.get($index)
-					.ok_or(CommentReadError::EndOfPacket)?
-			};
-		}
-
 		let mut user_comments = vec![];
 		let mut vendor_string = None;
 
 		// As stated, the Vorbis specification allows for an end of packet to happen while
-		// reading the comment header. To handle this elegantly, wrap the parsing code in
-		// a closure that can return early if EOP happens
-		match (|| -> Result<(), CommentReadError> {
-			// Read the vendor string
-			let vendor_string_length = usize::try_from(u32::from_le_bytes(
-				get_packet_checked!(..4).try_into().unwrap()
-			))?;
-			// The Vorbis specification mandates this string to be encoded in UTF-8, but
-			// we don't enforce that here because some encoders and Vorbis manipulation
-			// tools do not respect that. We could do a lossy UTF-8 conversion here to
-			// ensure we always output valid UTF-8, but that would lose information about
-			// the offending characters, so no program would be able to read the original
-			// string in any encoding. Moreover, the most broken decoders probably won't
-			// handle UTF-8 properly and choke with the Unicode replacement character. So
-			// treat this as an opaque byte string and let broken decoders deal with their
-			// own brokenness. Good decoders are very likely to be made by programmers who
-			// paid attention to these matters like us, and handle the error by doing a
-			// lossy conversion for display, or signalling the appropriate warnings or
-			// errors. In any case, we only append valid UTF-8 bytes to this string, so it
-			// won't happen that valid vendor strings are made non-conforming by us.
-			//
-			// As a side benefit, skipping UTF-8 validation is a bit faster
-			let raw_vendor_string = get_packet_checked!(4..4 + vendor_string_length);
-
-			info!(
-				"Encoder vendor string: {}",
-				String::from_utf8_lossy(raw_vendor_string)
-			);
-
-			vendor_string = Some(match settings.vendor_string_action {
-				VorbisVendorStringAction::Copy => raw_vendor_string.into(),
-				VorbisVendorStringAction::Replace => OPTIVORBIS_VERSION_TAG.into(),
-				VorbisVendorStringAction::AppendTag => {
-					append_tag_if_needed(raw_vendor_string, OPTIVORBIS_VERSION_TAG)
-				}
-				VorbisVendorStringAction::AppendShortTag => {
-					append_tag_if_needed(raw_vendor_string, OPTIVORBIS_SHORT_VERSION_TAG)
-				}
-				VorbisVendorStringAction::Empty => "".into()
-			});
-
-			let mut user_comment_count = u32::from_le_bytes(
-				get_packet_checked!(4 + vendor_string_length..4 + vendor_string_length + 4)
-					.try_into()
-					.unwrap()
-			);
-
-			info!("User comment count: {}", user_comment_count);
-
-			// Now read the user comment fields if they should be copied
-			if settings.comment_fields_action == VorbisCommentFieldsAction::Copy {
-				trace!("Copying user comments");
-
-				let mut user_comment_length_start_index = 4 + vendor_string_length + 4;
-
-				while user_comment_count > 0 {
-					let user_comment_length_end_index = user_comment_length_start_index + 4;
-					let user_comment_length = usize::try_from(u32::from_le_bytes(
-						get_packet_checked!(
-							user_comment_length_start_index..user_comment_length_end_index
-						)
-						.try_into()
-						.unwrap()
-					))?;
-					let user_comment_end_index =
-						user_comment_length_end_index + user_comment_length;
-
-					// We don't do any validation on the actual contents of the comments because not
-					// all encoders follow the specification, and we don't care about their contents
-					// anyway. Even UTF-8 validity can't be taken for granted, as explained above
-					// with the vendor strings
-					let user_comment =
-						get_packet_checked!(user_comment_length_end_index..user_comment_end_index);
-
-					info!("User comment: {}", String::from_utf8_lossy(user_comment));
-
-					user_comments.push(user_comment.into());
-
-					user_comment_length_start_index = user_comment_end_index;
-					user_comment_count -= 1;
-				}
-			} else {
-				trace!("Skipping user comments");
-			}
-
-			Ok(())
-		})() {
+		// reading the comment header. Therefore, we need to handle that condition specifically
+		match parse(
+			comment_header,
+			settings,
+			&mut vendor_string,
+			&mut user_comments
+		) {
 			Err(CommentReadError::OptimizerError(err)) => return Err(err),
 			Err(CommentReadError::EndOfPacket) => {
 				// Most programs won't accept this kind of Vorbis streams, so let the user
@@ -194,6 +106,104 @@ impl CommentHeaderParse {
 			})
 		))
 	}
+}
+
+/// Parses the specified comment header, extracting the vendor string and user comments according
+/// to the settings.
+fn parse(
+	comment_header: &[u8],
+	settings: &VorbisOptimizerSettings,
+	vendor_string: &mut Option<Vec<u8>>,
+	user_comments: &mut Vec<Vec<u8>>
+) -> Result<(), CommentReadError> {
+	macro_rules! get_packet_checked {
+		($index:expr) => {
+			comment_header
+				.get($index)
+				.ok_or(CommentReadError::EndOfPacket)?
+		};
+	}
+
+	// Read the vendor string
+	let vendor_string_length = usize::try_from(u32::from_le_bytes(
+		get_packet_checked!(..4).try_into().unwrap()
+	))?;
+	// The Vorbis specification mandates this string to be encoded in UTF-8, but
+	// we don't enforce that here because some encoders and Vorbis manipulation
+	// tools do not respect that. We could do a lossy UTF-8 conversion here to
+	// ensure we always output valid UTF-8, but that would lose information about
+	// the offending characters, so no program would be able to read the original
+	// string in any encoding. Moreover, the most broken decoders probably won't
+	// handle UTF-8 properly and choke with the Unicode replacement character. So
+	// treat this as an opaque byte string and let broken decoders deal with their
+	// own brokenness. Good decoders are very likely to be made by programmers who
+	// paid attention to these matters like us, and handle the error by doing a
+	// lossy conversion for display, or signalling the appropriate warnings or
+	// errors. In any case, we only append valid UTF-8 bytes to this string, so it
+	// won't happen that valid vendor strings are made non-conforming by us.
+	//
+	// As a side benefit, skipping UTF-8 validation is a bit faster
+	let raw_vendor_string = get_packet_checked!(4..4 + vendor_string_length);
+
+	info!(
+		"Encoder vendor string: {}",
+		String::from_utf8_lossy(raw_vendor_string)
+	);
+
+	*vendor_string = Some(match settings.vendor_string_action {
+		VorbisVendorStringAction::Copy => raw_vendor_string.into(),
+		VorbisVendorStringAction::Replace => OPTIVORBIS_VERSION_TAG.into(),
+		VorbisVendorStringAction::AppendTag => {
+			append_tag_if_needed(raw_vendor_string, OPTIVORBIS_VERSION_TAG)
+		}
+		VorbisVendorStringAction::AppendShortTag => {
+			append_tag_if_needed(raw_vendor_string, OPTIVORBIS_SHORT_VERSION_TAG)
+		}
+		VorbisVendorStringAction::Empty => "".into()
+	});
+
+	let mut user_comment_count = u32::from_le_bytes(
+		get_packet_checked!(4 + vendor_string_length..4 + vendor_string_length + 4)
+			.try_into()
+			.unwrap()
+	);
+
+	info!("User comment count: {}", user_comment_count);
+
+	// Now read the user comment fields if they should be copied
+	if settings.comment_fields_action == VorbisCommentFieldsAction::Copy {
+		trace!("Copying user comments");
+
+		let mut user_comment_length_start_index = 4 + vendor_string_length + 4;
+
+		while user_comment_count > 0 {
+			let user_comment_length_end_index = user_comment_length_start_index + 4;
+			let user_comment_length = usize::try_from(u32::from_le_bytes(
+				get_packet_checked!(user_comment_length_start_index..user_comment_length_end_index)
+					.try_into()
+					.unwrap()
+			))?;
+			let user_comment_end_index = user_comment_length_end_index + user_comment_length;
+
+			// We don't do any validation on the actual contents of the comments because not
+			// all encoders follow the specification, and we don't care about their contents
+			// anyway. Even UTF-8 validity can't be taken for granted, as explained above
+			// with the vendor strings
+			let user_comment =
+				get_packet_checked!(user_comment_length_end_index..user_comment_end_index);
+
+			info!("User comment: {}", String::from_utf8_lossy(user_comment));
+
+			user_comments.push(user_comment.into());
+
+			user_comment_length_start_index = user_comment_end_index;
+			user_comment_count -= 1;
+		}
+	} else {
+		trace!("Skipping user comments");
+	}
+
+	Ok(())
 }
 
 /// Appends the given tag to the given binary string if it doesn't already contain it.

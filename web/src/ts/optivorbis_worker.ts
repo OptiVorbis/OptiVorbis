a95c74e93c99fa136d5771c957dbd33747596885
Alejandro González <me@alegon.dev>
2025-09-20 17:55:08 +0200

    fix(web): address Typescript 5.7 change
    Was it more of a regression for this purpose? See:
    https://www.typescriptlang.org/docs/handbook/release-notes/typescript-5-7.html#support-for---target-es2024-and---lib-es2024

--- web/src/ts/optivorbis_worker.ts
+++ web/src/ts/optivorbis_worker.ts
@@ -3,7 +3,7 @@ import { OggToOgg } from 'optivorbis';
 self.addEventListener('message', (ev: MessageEvent<File>) => {
   try {
     const vorbisFileData = new Uint8Array(new FileReaderSync().readAsArrayBuffer(ev.data));
-    const optimizedVorbisFileData = new OggToOgg().remux(vorbisFileData);
+    const optimizedVorbisFileData = new OggToOgg().remux(vorbisFileData) as Uint8Array<ArrayBuffer>;
     self.postMessage(new Blob([optimizedVorbisFileData], { type: 'audio/ogg' }));
   } catch (remuxErrorMessage) {
     // Error handling in web workers is finicky, so we pass an error message to the main thread
